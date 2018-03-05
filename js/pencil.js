884560958ffaea76e71c01acffe4a7ebf886a9a7
wonghan <han.wong@hotmail.com>
2018-03-05 22:17:14 +0800

    3-jest-getstart

--- js/pencil.js
+++ js/pencil.js
@@ -1,13 +1,15 @@
 /* global query */
-
+const Pencil = {}
 /**
  * 反序列化 querystring
  * @param {String} url
  * @return {undefined|String}
  */
-function query (name, url) {
+Pencil.query =(name, url)=> {
   // ?hello=test&wonghan=handsome
   var reg = new RegExp('(?:\\?|&)' + name + '=(.*?)(?:&|$)')
   var ret = reg.exec(url) || []
   return ret[1]
 }
+
+module.exports = Pencil
