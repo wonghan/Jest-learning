3dd376c17cc56c23c27b8bfb2b402fc745668ed6
wonghan <han.wong@hotmail.com>
2018-03-06 00:09:27 +0800

    4-jest-config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // testMatch: ['**/__tests__/**/*.js?(x)','**/?(*.)(spec|test).js?(x)'],
+    // testRegex: '(/__tests__).*|(\\.|/)(test|spec))\\.jsx?$',
+    testMatch: ['<rootDir>/test/**/*.js'],
+    testEnvironment: 'jsdom',
+    rootDir: '',
+    moduleFileExtensions: ['js','json','jsx','node']
+}
